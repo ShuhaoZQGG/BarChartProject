dc577ae11f09d5a84553f7b6039246b383ecc818
unknown <shuhao.zhang@mail.mcgill.ca>
2021-08-25 13:01:11 -0400

    add react app

---
+++ bar-chart-project/src/App.js
@@ -0,0 +1,11 @@
+import { Framework } from './components/Framework';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Framework/>
+    </div>
+  );
+}
+
+export default App;
---
+++ bar-chart-project/src/components/Framework.js
@@ -0,0 +1,12 @@
+export const Framework = () => {
+  return (
+    <div>
+      <h1 style = {framworkStyle}>Bar Chart Project</h1>
+    </div>
+  )
+}
+
+const framworkStyle = {
+  color: 'blue',
+  backgroundColor: 'black'
+}
